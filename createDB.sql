f8d77adf63e76eb94cab237bbea9edcca7a0a81a
Dinar <dinar@reaspekt.com>
2021-03-29 17:50:18 +0300

    Должно помочь с битой кодировкой

--- createDB.sql
+++ createDB.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE IF NOT EXISTS books CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE books;
+SET NAMES utf8mb4;
 DROP TABLE IF EXISTS book;
 CREATE TABLE book(id INT PRIMARY KEY AUTO_INCREMENT, title VARCHAR(100) NOT NULL, author VARCHAR(100) NOT NULL, publication_date DATE, number_pages INTEGER);
 INSERT book(title, author, publication_date, number_pages) VALUES ("Check", "me", "1994-10-12", 324), ("PROBA", "Check", "2015-12-12", 456), ("Rugrats", "Andrew Sapkovski", "2010-01-06", 300);
